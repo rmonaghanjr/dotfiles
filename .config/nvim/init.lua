ace8da4f6dfa22d53d9ad47d3876ea4d778a76bf
rmonaghanjr <rmonagha@stevens.edu>
2023-10-08 15:58:48 -0400

    add: initProj script

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -82,17 +82,44 @@ require('lazy').setup({
 
     -- "gc" to comment visual regions/lines
     { 'numToStr/Comment.nvim', opts = {} },
+    {
+        'nvim-telescope/telescope-fzf-native.nvim',
+        -- NOTE: If you are having trouble with this installation,
+        --       refer to the README for telescope-fzf-native for more instructions.
+        build = 'make',
+    },
+
 
     -- Fuzzy Finder (files, lsp, etc)
     {
         'nvim-telescope/telescope.nvim',
-        version = '*',
         dependencies = {
             'nvim-lua/plenary.nvim',
             'debugloop/telescope-undo.nvim'
         },
         config = function()
             require("telescope").setup({
+                defaults = {
+                    mappings = {
+                        i = {
+                            ['<C-u>'] = false,
+                            ['<C-d>'] = false,
+                        },
+                    },
+                    file_ignore_patterns = {
+                        "node%_modules/*",
+                        "target/*",
+                        "dist/*"
+                    },
+                    find_command = {
+                        "rg",
+                        "--no-heading",
+                        "--with-filename",
+                        "--line-number",
+                        "--column",
+                        "--smart-case",
+                    },
+                },
                 extensions = {
                     undo = {
                         side_by_side = true,
@@ -101,9 +128,17 @@ require('lazy').setup({
                             preview_height = 0.9,
                         },
                     },
+                    fzf = {
+                        fuzzy = true,                    -- false will only do exact matching
+                        override_generic_sorter = true,  -- override the generic sorter
+                        override_file_sorter = true,     -- override the file sorter
+                        case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+                        -- the default case_mode is "smart_case"
+                    }
                 },
             })
             require("telescope").load_extension("undo")
+            require("telescope").load_extension("fzf")
 
             vim.keymap.set("n", "<leader>u", "<cmd>Telescope undo<cr>")
         end,
@@ -112,13 +147,6 @@ require('lazy').setup({
     -- Fuzzy Finder Algorithm which requires local dependencies to be built.
     -- Only load if `make` is available. Make sure you have the system
     -- requirements installed.
-    {
-        'nvim-telescope/telescope-fzf-native.nvim',
-        -- NOTE: If you are having trouble with this installation,
-        --       refer to the README for telescope-fzf-native for more instructions.
-        build = 'make',
-    },
-
     { -- Highlight, edit, and navigate code
         'nvim-treesitter/nvim-treesitter',
         dependencies = {
@@ -270,25 +298,6 @@ vim.api.nvim_create_autocmd('FileType', {
 
 -- [[ Configure Telescope ]]
 -- See `:help telescope` and `:help telescope.setup()`
-require('telescope').setup {
-    defaults = {
-        mappings = {
-            i = {
-                ['<C-u>'] = false,
-                ['<C-d>'] = false,
-            },
-        },
-        file_ignore_patterns = {
-            "node%_modules/*",
-            "target/*",
-            "dist/*"
-        }
-    },
-}
-
--- Enable telescope fzf native, if installed
-pcall(require('telescope').load_extension, 'fzf')
-
 -- See `:help telescope.builtin`
 vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
 vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
